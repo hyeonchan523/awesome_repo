4c577222e100986e96cdf6fe9f48fa1daedded7d
hyeonchan lee <hyeonchan523@gmail.com>
2025-07-16 13:14:40 +0900

    make functions class

--- engine.py
+++ engine.py
@@ -1,6 +1,26 @@
-def plus(a,b):
-    return a+b
-def multiply(a,b):
-    return a*b
-def minus(a,b):
-    return a-b
+class Calculator:
+    def __init__(self):
+        self.result = 0
+
+    def add(self, value):
+        self.result += value
+        return self.result
+
+    def subtract(self, value):
+        self.result -= value
+        return self.result
+
+    def multiply(self, value):
+        self.result *= value
+        return self.result
+
+    def divide(self, value):
+        if value != 0:
+            self.result /= value
+        else:
+            raise ValueError("Cannot divide by zero")
+        return self.result
+    
+    def reset(self):
+        self.result = 0
+        return self.result
